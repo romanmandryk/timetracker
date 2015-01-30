d68afb8c508ad43b4ddc0fef600c1f30cfe3bc46
Roman Mandryk <roman.mandryk@gmail.com>
2015-01-30 22:10:42 +1100

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('tttimeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
